97c558c0d77a9101b8556585b3f080a5d31dcde8
schochastics <david@schochastics.net>
2025-03-26 21:54:43 +0100

    added fromat argument to format

--- R/hms.R
+++ R/hms.R
@@ -166,14 +166,22 @@ as.hms.default <- function(x, ...) {
 #'   [pkgconfig::set_config()].
 #' @export
 #' @importFrom pkgconfig get_config
-as.hms.POSIXt <- function(x, tz = pkgconfig::get_config("hms::default_tz", ""), ...) {
+as.hms.POSIXt <- function(
+  x,
+  tz = pkgconfig::get_config("hms::default_tz", ""),
+  ...
+) {
   time <- as.POSIXlt(x, tz = tz)
   vec_cast(time, new_hms())
 }
 
 #' @rdname Deprecated
 #' @export
-as.hms.POSIXlt <- function(x, tz = pkgconfig::get_config("hms::default_tz", ""), ...) {
+as.hms.POSIXlt <- function(
+  x,
+  tz = pkgconfig::get_config("hms::default_tz", ""),
+  ...
+) {
   # We need to roundtrip via as.POSIXct() to respect the time zone
   time <- as.POSIXlt(as.POSIXct(x), tz = tz)
   vec_cast(time, new_hms())
@@ -204,12 +212,19 @@ as.character.hms <- function(x, ...) {
 format_hms <- function(x) {
   xx <- decompose(x)
 
-  ifelse(is.na(x), NA_character_, paste0(
-    ifelse(xx$sign, "-", ""),
-    format_hours(xx$hours), ":",
-    format_two_digits(xx$minute_of_hour), ":",
-    format_two_digits(xx$second_of_minute),
-    format_tics(xx$tics)))
+  ifelse(
+    is.na(x),
+    NA_character_,
+    paste0(
+      ifelse(xx$sign, "-", ""),
+      format_hours(xx$hours),
+      ":",
+      format_two_digits(xx$minute_of_hour),
+      ":",
+      format_two_digits(xx$second_of_minute),
+      format_tics(xx$tics)
+    )
+  )
 }
 
 
@@ -258,13 +273,71 @@ c.hms <- function(x, ...) {
 
 # Output ------------------------------------------------------------------
 
+decimal_digits <- function(x) {
+  sapply(x, function(num) {
+    if (is.na(num) || num %% 1 == 0) return(0)
+    # Convert to character with full precision, strip trailing zeros
+    dec_str <- sub(
+      "^[^.]*\\.",
+      "",
+      sub("0+$", "", format(num, scientific = FALSE, trim = TRUE))
+    )
+    nchar(dec_str)
+  })
+}
+
 #' @rdname hms
 #' @export
-format.hms <- function(x, ...) {
+format.hms <- function(x, format = "H:m:s_dec", digits, ...) {
   if (length(x) == 0L) {
     "hms()"
   } else {
-    format(as.character(x), justify = "right")
+    secs_all <- as.numeric(x)
+    is_neg <- secs_all < 0
+    is_neg[is.na(is_neg)] <- FALSE
+    has_digit <- any(secs_all %% 1 != 0, na.rm = TRUE)
+    if (has_digit && missing(digits)) {
+      digits <- min(max(decimal_digits(secs_all)), 6)
+    } else if (!has_digit && missing(digits)) {
+      digits <- 0
+    }
+    secs_all <- abs(secs_all)
+    pad2 <- function(n) sprintf("%02d", n)
+
+    format_one <- function(secs, digits) {
+      if (is.na(secs)) {
+        return("NA")
+      }
+      h <- floor(secs / 3600)
+      m <- floor((secs %% 3600) / 60)
+      # s <- floor(secs %% 60)
+      s_dec <- secs %% 60
+      s_dec_str <- formatC(
+        s_dec,
+        format = "f",
+        digits = digits,
+        flag = "0"
+      )
+      if (s_dec >= 0 && s_dec < 10) {
+        s_dec_str <- paste0("0", s_dec_str)
+      }
+      out <- format
+      out <- gsub("s_dec", s_dec_str, out, fixed = TRUE)
+      out <- gsub("H", pad2(h), out, fixed = TRUE)
+      out <- gsub("m", pad2(m), out, fixed = TRUE)
+      # out <- gsub("s", pad2(s), out, fixed = TRUE)
+
+      out
+    }
+
+    raw_vals <- vapply(secs_all, format_one, character(1), digits = digits)
+    max_width <- max(nchar(raw_vals, type = "width"))
+    non_neg_vals <- sprintf(paste0("%", max_width, "s"), raw_vals)
+    if (any(is_neg, na.rm = TRUE)) {
+      prefix <- ifelse(is_neg, "-", " ")
+      return(paste0(prefix, non_neg_vals))
+    }
+    non_neg_vals
   }
 }
 
